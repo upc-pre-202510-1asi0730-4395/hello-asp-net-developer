c93bd9ed263d0d5ef31d2ea6987320cbf1b5fe47
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-09 13:34:37 -0500

    feat(main): main behavior.

--- Acme.Hello.Platform/Program.cs
+++ Acme.Hello.Platform/Program.cs
@@ -1,10 +1,19 @@
+using Acme.Hello.Platform.Generic.Domain.Model.Entities;
+using Acme.Hello.Platform.Generic.Interfaces.REST.Assemblers;
+using Acme.Hello.Platform.Generic.Interfaces.REST.Resources;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -12,30 +21,36 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
+// /// <summary>
+// /// Defines the GET endpoint for the greeting.
+// /// </summary>
+// /// <param name="firstName">The first name of the developer.</param>
+// /// <param name="lastName">The last name of the developer.</param>
+// /// <returns>A response containing the greeting message.</returns>
+app.MapGet("/greetings", (string? firstName, string? lastName) =>
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
+        var developer = !string.IsNullOrWhiteSpace(firstName) &&
+                        !string.IsNullOrWhiteSpace(lastName)
+            ? new Developer(firstName, lastName)
+            : null;
+        var response = GreetDeveloperAssembler.ToResponseFromEntity(developer);
+        return Results.Ok(response);
     })
-    .WithName("GetWeatherForecast");
+    .WithName("GetGreeting")
+    .WithOpenApi();
 
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+// /// <summary>
+// /// Defines the POST endpoint for creating a greeting.
+// /// </summary>
+// /// <param name="request">The request containing the developer's first and last name.</param>
+// /// <returns>A response containing the greeting message.</returns>
+app.MapPost("/greetings", (GreetDeveloperRequest request) =>
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+    var developer = DeveloperAssembler.ToEntityFromRequest(request);
+    var response = GreetDeveloperAssembler.ToResponseFromEntity(developer);
+    return Results.Created("/greetings", response);
+})
+.WithName("CreateGreeting")
+.WithOpenApi();
+
+app.Run();    
